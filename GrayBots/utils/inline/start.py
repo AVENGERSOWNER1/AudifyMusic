ea12378a298c8dcc32b52f100ba1fd18456c4d62
GrayBots <yourxnikhil@gmail.com>
2025-06-18 15:15:11 +0530

    AudifyMusic

--- GrayBots/utils/inline/start.py
+++ GrayBots/utils/inline/start.py
@@ -29,8 +29,7 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true"
             )
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID), 
+        [ 
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
 
@@ -39,7 +38,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL)
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], web_app=WebAppInfo(url="https://vps-kappa.vercel.app")), 
+            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID), 
             InlineKeyboardButton(text=_["S_B_5"], url="https://github.com/GrayBots/AudifyMusic")
         ]
     ]
